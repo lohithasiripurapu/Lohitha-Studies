e7d5a91bc553e8da2278d6cf09ea4359c5cae3a5
Lohitha <lohitha.siripurapu@gmail.com>
2022-06-08 22:34:29 +0530

    format todo.js

--- js/todos.js
+++ js/todos.js
@@ -1,36 +1,33 @@
-let input = "temp"
-let todo=[]
-while (input!="quit"){
-    input = prompt("What would you like to do?")
-    switch(input){
-        case "new": todo.push(prompt("What do you want to add"))
-        break
-        case "delete":
-            
-        //console.log("Select the index you want to delete")
-        for(let i=0;i<todo.length;i++){
-            console.log(`${i} : ${todo[i]}`)
-        }
+let input = "temp";
+let todo = [];
+while (input != "quit") {
+  input = prompt("What would you like to do?");
+  switch (input) {
+    case "new":
+      todo.push(prompt("What do you want to add"));
+      break;
+    case "delete":
+      //console.log("Select the index you want to delete")
+      for (let i = 0; i < todo.length; i++) {
+        console.log(`${i} : ${todo[i]}`);
+      }
 
-        let rem=prompt("Select the index you want to delete")
-        todo.splice(rem,1)
+      let rem = prompt("Select the index you want to delete");
+      todo.splice(rem, 1);
 
-        console.log("After delete")
-        for(let i=0;i<todo.length;i++){
-            console.log(`${i} : ${todo[i]}`)
-        }
-        break
-    
+      console.log("After delete");
+      for (let i = 0; i < todo.length; i++) {
+        console.log(`${i} : ${todo[i]}`);
+      }
+      break;
 
-        case "list":
-            console.log("Here is the list of all elements in todo till now")
-            for(let i=0;i<todo.length;i++){
-                console.log(`${i} : ${todo[i]}`)
-            }
+    case "list":
+      console.log("Here is the list of all elements in todo till now");
+      for (let i = 0; i < todo.length; i++) {
+        console.log(`${i} : ${todo[i]}`);
+      }
 
-            break
-    }
-
-     
+      break;
+  }
 }
-console.log("quit the app")
+console.log("quit the app");
